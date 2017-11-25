db2692d58a52687a861fd49aa3e5851c81e5760a
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-11-24 19:32:02 -0800

    completed day2 in python

--- 2016/python/src/day2.py
+++ 2016/python/src/day2.py
@@ -3,6 +3,7 @@
 Advent of Code 2016: Day 2
 """
 import os
+debugging = "RRDDD"
 testinstructions = """ULL
 RRDDD
 LURDL
@@ -17,6 +18,9 @@ def readdayinput():
     dayinputfile = "../resources/{}input.txt".format(thisfile)
     with open(dayinputfile, mode='r', encoding='utf-8') as fileio:
         dayinput = fileio.read()
+    dayinput = dayinput.strip('\n')
+    #dayinput = debugging
+    #dayinput = testinstructions
     return dayinput
 
 def solvebathroom(dayinput):
@@ -24,6 +28,7 @@ def solvebathroom(dayinput):
     first half solver:
     push buttons on keypad
     """
+    code = []
     cases = dayinput.split('\n')
     positions = [x for x in range(1, 10)]
     index = 4
@@ -38,8 +43,45 @@ def solvebathroom(dayinput):
                 index += 1
             elif d == "L" and index % 3 != 0:
                 index -= 1
-        print(positions[index], end="")
+        code.append(str(positions[index]))
+    print(''.join(code))
+
+
+def solvediamond(dayinput):
+    """
+    second half solver:
+    push buttons on diamond keypad
+    """
+    code = []
+    cases = dayinput.split('\n')
+    positions = [x for x in range(1, 10)] + ['A', 'B', 'C', 'D']
+    index = 4
+    restrictions = {
+        "U": [0, 1, 3, 4, 8],
+        "D": [4, 8, 9, 11, 12],
+        "R": [0, 3, 8, 11, 12],
+        "L": [0, 1, 4, 9, 12]
+    }
     print()
+    for case in cases:
+        for d in case:
+            if d == "U" and index not in restrictions[d]:
+                if index in [2, 12]:
+                    index -= 2
+                else:
+                    index -= 4
+            elif d == "D" and index not in restrictions[d]:
+                if index in [0, 10]:
+                    index += 2
+                else:
+                    index += 4
+            elif d == "R" and index not in restrictions[d]:
+                index += 1
+            elif d == "L" and index not in restrictions[d]:
+                index -= 1
+        code.append(str(positions[index]))
+    print(''.join(code))
+
 
 if __name__ == "__main__":
     """
@@ -47,3 +89,4 @@ if __name__ == "__main__":
     """
     dayinput = readdayinput()
     solvebathroom(dayinput)
+    solvediamond(dayinput)
