d4f1fd26b18432bab0fc2cb2df186e25ed4340c2
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-12-03 09:29:39 -0800

    day3 half one complete

--- 2017/python/src/day3.py
+++ 2017/python/src/day3.py
@@ -4,6 +4,14 @@ Advent of Code 2017: Day #
 """
 import os
 
+test = [
+    [17,  16,  15,  14,  13],
+    [18,   5,   4,   3,  12],
+    [19,   6,   1,   2,  11],
+    [20,   7,   8,   9,  10],
+    [21,  22,  23,  24,  25]
+]
+
 def readdayinput():
     """
     Reads day input to solve
@@ -21,21 +29,55 @@ def readdayinput():
     dayinput = dayinput.strip('\n')
     return dayinput
 
-
 def first_half(dayinput):
     """
     first half solver:
     """
-    lines = dayinput.split('\n')
-    result = None
-
-    return result
+    loop = [[1]]
+    n = 2
+    while n < dayinput:
+        # right
+        end = len(loop) + 1
+        while len(loop[-1]) < end:
+            loop[-1].append(n)
+            n += 1
+        # up
+        i = len(loop) - 2
+        while i >= 0:
+            loop[i].append(n)
+            n += 1
+            i -= 1
+        loop.insert(0, [n])
+        n += 1
+        # left
+        while len(loop[0]) < len(loop) + 1:
+            loop[0].insert(0, n)
+            n += 1
+        # down
+        i = 1
+        while i < len(loop[0]) - 1:
+            loop[i].insert(0, n)
+            n += 1
+            i += 1
+        loop.append([n])
+        n += 1
+    i = 0
+    while i < len(loop):
+        if dayinput in loop[i]:
+            row, col = i, loop[i].index(dayinput)
+            total_r, total_c = len(loop), len(loop[i])
+            break
+        i += 1
+    half = total_r // 2
+    steps = abs(half - row)
+    half = total_c // 2
+    steps += abs(half - col)
+    return steps
 
 def second_half(dayinput):
     """
     second half solver:
     """
-    lines = dayinput.split('\n')
     result = None
 
     return result
@@ -44,10 +86,12 @@ def app():
     """
     runs day application
     """
-    dayinput = readdayinput()
+    dayinput = 361527 # readdayinput()
     half_one = first_half(dayinput)
     half_two = second_half(dayinput)
     print(half_one, half_two)
+
+
 
 if __name__ == "__main__":
     """
