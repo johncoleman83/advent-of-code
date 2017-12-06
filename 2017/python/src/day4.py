99f6c257787633c1726c6f28bd73e7538f1e68bf
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-12-06 11:02:50 -0800

    completed day4

--- 2017/python/src/day4.py
+++ 2017/python/src/day4.py
@@ -27,16 +27,28 @@ def first_half(dayinput):
     first half solver:
     """
     lines = dayinput.split('\n')
-    result = None
+    correct = 0
+    for line in lines:
+        if len(line.split(' ')) == len(set(line.split(' '))):
+            correct += 1
 
-    return result
+    return correct
 
 def second_half(dayinput):
     """
     second half solver:
     """
     lines = dayinput.split('\n')
-    result = None
+    correct = 0
+    for line in lines:
+        word_set = set()
+        line = line.split(' ')
+        for word in line:
+            word_set.add(''.join(sorted(list(word))))
+        if len(line) == len(set(word_set)):
+            correct += 1
+
+    return correct
 
     return result
 
