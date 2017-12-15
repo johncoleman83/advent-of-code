332e82e5c7e9758321cbc4b01fecbb20d8bd0695
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-12-14 19:01:35 -0800

    completed day 10 & 14

--- 2017/python/src/day10.py
+++ 2017/python/src/day10.py
@@ -26,19 +26,92 @@ def first_half(dayinput):
     """
     first half solver:
     """
-    lines = dayinput.split('\n')
-    result = None
+    knot = [i for i in range(256)]
+    #knot = [0, 1, 2, 3, 4]
+    sub_lengths = [int(i) for i in dayinput.split(',')]
+    #sub_lengths = [3, 4, 1, 5]
+    current = skip = 0
+    for length in sub_lengths:
+        skip_base, index, counter = length, current, 0
+        sub = []
+        while counter < length:
+            if index == len(knot):
+                index = 0
+            sub.append(knot[index])
+            index += 1
+            counter += 1
+        sub = list(reversed(sub))
+        index, counter = current, 0
+        while counter < length:
+            if index == len(knot):
+                index = 0
+            knot[index] = sub[counter]
+            index += 1
+            counter += 1
+        current += skip_base + skip
+        while current >= len(knot):
+            current = current - len(knot)
+        skip += 1
 
-    return result
+    return knot[0] * knot[1]
+
 
 def second_half(dayinput):
     """
     second half solver:
     """
-    lines = dayinput.split('\n')
-    result = None
+    suffix = [17, 31, 73, 47, 23]
+    knot = [i for i in range(256)]
+    sub_lengths = []
+    for x in dayinput:
+        sub_lengths.append(ord(x))
+    print(sub_lengths)
+    sub_lengths += suffix
+    current = skip = 0
+    for x in range(64):
+        for length in sub_lengths:
+            skip_base, index, counter = length, current, 0
+            sub = []
+            while counter < length:
+                if index == len(knot):
+                    index = 0
+                sub.append(knot[index])
+                index += 1
+                counter += 1
+            sub = list(reversed(sub))
+            index, counter = current, 0
+            while counter < length:
+                if index == len(knot):
+                    index = 0
+                knot[index] = sub[counter]
+                index += 1
+                counter += 1
+            current += skip_base + skip
+            while current >= len(knot):
+                current = current - len(knot)
+            skip += 1
+    print(knot)
+    dense_hash = []
+    index = 0
+    while index < len(knot):
+        value = knot[index]
+        index += 1
+        count = 0
+        while count < 15:
+            value ^= knot[index]
+            index += 1
+            count += 1
+        dense_hash.append(value)
+    print(dense_hash)
+    knot_hash = []
+    for val in dense_hash:
+        hex_val = hex(val).split('x')[-1]
+        if len(hex_val) == 1:
+            hex_val = '0' + hex_val
+        knot_hash.append(hex_val)
 
-    return result
+    return ''.join(knot_hash)
+
 
 def app():
     """
--- 2017/python/src/day14.py
+++ 2017/python/src/day14.py
@@ -3,6 +3,7 @@
 Advent of Code 2017: Day #
 """
 import os
+#from gridday14 import GRID
 
 def readdayinput():
     """
@@ -21,32 +22,123 @@ def readdayinput():
     dayinput = dayinput.strip('\n')
     return dayinput
 
+def knot_hash(dayinput):
+    """
+    second half solver day 10:
+    """
+    suffix = [17, 31, 73, 47, 23]
+    knot = [i for i in range(256)]
+    sub_lengths = []
+    for x in dayinput:
+        sub_lengths.append(ord(x))
+    sub_lengths += suffix
+    current = skip = 0
+    for x in range(64):
+        for length in sub_lengths:
+            skip_base, index, counter = length, current, 0
+            sub = []
+            while counter < length:
+                if index == len(knot):
+                    index = 0
+                sub.append(knot[index])
+                index += 1
+                counter += 1
+            sub = list(reversed(sub))
+            index, counter = current, 0
+            while counter < length:
+                if index == len(knot):
+                    index = 0
+                knot[index] = sub[counter]
+                index += 1
+                counter += 1
+            current += skip_base + skip
+            while current >= len(knot):
+                current = current - len(knot)
+            skip += 1
+    dense_hash = []
+    index = 0
+    while index < len(knot):
+        value = knot[index]
+        index += 1
+        count = 0
+        while count < 15:
+            value ^= knot[index]
+            index += 1
+            count += 1
+        dense_hash.append(value)
+    knot_hash = []
+    for val in dense_hash:
+        hex_val = hex(val).split('x')[-1]
+        if len(hex_val) == 1:
+            hex_val = '0' + hex_val
+        knot_hash.append(hex_val)
+    return ''.join(knot_hash)
+
+
+def hex_to_bin(h):
+    b = bin(int(h, 16))[2:]
+    while len(b) < 4:
+        b = '0' + b
+    return b
 
 def first_half(dayinput):
     """
     first half solver:
     """
-    lines = dayinput.split('\n')
-    result = None
-
-    return result
+    squares_used = 0
+    for x in range(128):
+        k_hash = knot_hash("{}{}{}".format(dayinput, '-', str(x)))
+        #row = []
+        for h in k_hash:
+            b = hex_to_bin(h)
+            #row.append(b)
+            squares_used += b.count('1')
+        #row = ''.join(row)
+        #squares_used += row.count('1')
+    return squares_used
 
 def second_half(dayinput):
     """
     second half solver:
     """
-    lines = dayinput.split('\n')
-    result = None
+    squares_used = 0
+    grid = []
+    for x in range(128):
+        k_hash = knot_hash("{}{}{}".format(dayinput, '-', str(x)))
+        row = []
+        for h in k_hash:
+            b = hex_to_bin(h)
+            for b_digit in b:
+                row.append(int(b_digit))
+        grid.append(row)
+    return grid
 
-    return result
+
+def count_total_sections(grid):
+    def mutate_section(row, col):
+        if 0 <= row < len(grid) and 0 <= col < len(grid[row]) and grid[row][col] == 1:
+            grid[row][col] = 0
+            mutate_section(row + 1, col)
+            mutate_section(row - 1, col)
+            mutate_section(row, col + 1)
+            mutate_section(row, col - 1)
+            return 1
+        return 0
+    total_sections = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            total_sections += mutate_section(row, col)
+    return total_sections
+
 
 def app():
     """
     runs day application
     """
-    dayinput = readdayinput()
+    dayinput = 'ffayrhll' # readdayinput()
     half_one = first_half(dayinput)
-    half_two = second_half(dayinput)
+    GRID = second_half(dayinput)
+    half_two = count_total_sections(GRID)
     print(half_one, half_two)
 
 if __name__ == "__main__":
