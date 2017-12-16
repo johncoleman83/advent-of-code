8d748cd7b5d23043086dcbfc305d9b96dcd9d78a
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-12-15 21:41:35 -0800

    completed day16 rotating an array based on instructions 1 Billion times

--- 2017/python/src/day16.py
+++ 2017/python/src/day16.py
@@ -26,19 +26,55 @@ def first_half(dayinput):
     """
     first half solver:
     """
-    lines = dayinput.split('\n')
-    result = None
-
-    return result
+    positions = [x for x in 'abcdefghijklmnop']
+    lines = dayinput.split(',')
+    for move in lines:
+        if move[0] == 'x':
+            a, b = move[1:].split('/')
+            a, b = int(a), int(b)
+            positions[a], positions[b] = positions[b], positions[a]
+        elif move[0] == 'p':
+            a, b = move[1:].split('/')
+            ai, bi = positions.index(a), positions.index(b)
+            positions[ai], positions[bi] = positions[bi], positions[ai]
+        elif move[0] == 's':
+            spins = int(move[1:])
+            while spins > 0:
+                positions.insert(0, positions.pop())
+                spins -= 1
+    return ''.join(positions)
 
 def second_half(dayinput):
     """
+    'abcdefghijklmnop'
+    'lbdiomkhgcjanefp'
     second half solver:
     """
-    lines = dayinput.split('\n')
-    result = None
-
-    return result
+    positions = [x for x in 'abcdefghijklmnop']
+    lines = dayinput.split(',')
+    all_positions = {'abcdefghijklmnop'}
+    for x in range(48):
+        for move in lines:
+            if move[0] == 'x':
+                a, b = move[1:].split('/')
+                a, b = int(a), int(b)
+                positions[a], positions[b] = positions[b], positions[a]
+            elif move[0] == 'p':
+                a, b = move[1:].split('/')
+                ai, bi = positions.index(a), positions.index(b)
+                positions[ai], positions[bi] = positions[bi], positions[ai]
+            elif move[0] == 's':
+                spins = int(move[1:])
+                while spins > 0:
+                    positions.insert(0, positions.pop())
+                    spins -= 1
+        this_pos = ''.join(positions)
+        if this_pos in all_positions:
+            print(x, ''.join(positions))
+            return x
+        else:
+            all_positions.add(''.join(positions))
+    return ''.join(positions)
 
 def app():
     """
