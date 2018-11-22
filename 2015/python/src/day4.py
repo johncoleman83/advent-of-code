0ea162a66029aec55979bf7b551a86b06b049b22
johncoleman83 <djc_fileno@davidjohncoleman.com>
2018-11-22 11:17:31 -0800

    completed day4 2015

--- 2015/python/src/day4.py
+++ 2015/python/src/day4.py
@@ -3,33 +3,46 @@
 Advent of Code 2015: Day #
 """
 import os
-from shared.readdayinput import readdayinput
+import hashlib
 
-def first_half(dayinput):
+SECRET_KEY = 'iwrupvqb'
+
+def make_byte_string(n):
+    return "{}{}".format(SECRET_KEY, n).encode()
+
+def first_half():
     """
     first half solver:
+    find MD5 hashes which, in hexadecimal, start with at least five zeroes.
+    iwrupvqb followed by a number in decimal.
+    To mine AdventCoins, you must find Santa the lowest positive number that produces such a hash.
     """
-    lines = dayinput.split('\n')
-    result = None
+    n = 1
+    while n:
+        new_bytes = make_byte_string(n)
+        hash_to_check = hashlib.md5(new_bytes).hexdigest()
+        if hash_to_check[:5] == '00000':
+            return n
+        n += 1
 
-    return result
-
-def second_half(dayinput):
+def second_half():
     """
     second half solver:
     """
-    lines = dayinput.split('\n')
-    result = None
-
-    return result
+    n = 346386
+    while n:
+        new_bytes = make_byte_string(n)
+        hash_to_check = hashlib.md5(new_bytes).hexdigest()
+        if hash_to_check[:6] == '000000':
+            return n
+        n += 1
 
 def app():
     """
     runs day application
     """
-    dayinput = readdayinput()
-    half_one = first_half(dayinput)
-    half_two = second_half(dayinput)
+    half_one = first_half()
+    half_two = second_half()
     print(half_one, half_two)
 
 if __name__ == "__main__":
