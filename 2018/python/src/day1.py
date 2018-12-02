b24e7d1e7548ef2a48084ea2d376f9653cf978ee
John Coleman <jcoleman@pagerduty.com>
2018-12-01 20:58:34 -0800

    2018 day1

--- 2018/python/src/day1.py
+++ 2018/python/src/day1.py
@@ -8,20 +8,30 @@ from shared.readdayinput import readdayinput
 def first_half(dayinput):
     """
     first half solver:
+    Starting with a frequency of zero, what is the resulting
+    frequency after all of the changes in frequency have been applied?
     """
     lines = dayinput.split('\n')
-    result = None
-
+    result = 0
+    for freq in lines:
+        result += int(freq)
     return result
 
 def second_half(dayinput):
     """
     second half solver:
+    What is the first frequency your device reaches twice?
     """
     lines = dayinput.split('\n')
-    result = None
-
-    return result
+    freq_set = {0}
+    total_freq = 0
+    while True:
+        for freq in lines:
+            total_freq += int(freq)
+            if total_freq in freq_set:
+                return total_freq
+            freq_set.add(total_freq)
+    return None
 
 def app():
     """
